b8a521642d0a43353d7a82d30456d5ead5576e53
arturfil <arturfil@hotmail.com>
2021-12-14 03:25:15 -0500

    added validate revalidate and isAdmin

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,29 @@
 const express = require("express");
 const router = express.Router();
 
+const { check } = require('express-validator');
+
 const { loginUser, signUpUser } = require("../controllers/userController");
 const { validateJwt, revalidateJwt } = require("../helpers/processJwt");
+const { validateFields } = require("../helpers/validateFields");
 
-router.post("/login", loginUser);
-router.post("/signup", signUpUser)
+// POST/login
+router.post("/login", [
+  check("email", "Email field is required").isEmail(),
+  check("password", "Password field is required").not().isEmpty(),
+  validateFields
+],loginUser);
+
+// POST/signup
+router.post("/signup", [
+  check("name", "Name field is required").not().isEmpty(),
+  check("email", "Email field is required").isEmail(),
+  check("password", "Password must be 8 characters long with capital letter & Symbol")
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/, "i"),
+    validateFields
+], signUpUser);
+
+// POST/renew
 router.post("/renew", validateJwt, revalidateJwt);
 
 module.exports = router;
