9de69090435451a6d99c0936d347a65b9b5fe576
arturfil <arturfil@hotmail.com>
2021-12-14 01:50:32 -0500

    added validators and middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const { loginUser, signUpUser } = require("../controllers/userController")
+const { loginUser, signUpUser } = require("../controllers/userController");
+const { validateJwt, revalidateJwt } = require("../helpers/processJwt");
 
 router.post("/login", loginUser);
 router.post("/signup", signUpUser)
+router.post("/renew", validateJwt, revalidateJwt);
 
 module.exports = router;
